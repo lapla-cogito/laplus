f1c1ad984e49382910413ca015a84b4ef0cdc284
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-02-13 13:46:47 +0900

    メモリーマップを取得

--- laplusLoader/main.c
+++ laplusLoader/main.c
@@ -2,9 +2,21 @@
 #include  <Library/UefiLib.h>
 
 EFI_STATUS EFIAPI UefiMain(
-    EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
-  Print(L"Hello,World!\n laplus OS");
-  while (1);
-  return EFI_SUCCESS;
+	EFI_HANDLE image_handle,
+	EFI_SYSTEM_TABLE* system_table) {
+	Print(L"Hello,World!\n laplus OS");
+	while (1);
+	return EFI_SUCCESS;
+}
+
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+	if (map->buffer == NULL) { return EFI_BUFFER_TOO_SMALL; }
+
+	map->map_size = map->buffer_size;
+	return gBS->GetMemoryMap(
+		&map->map_size,
+		(EFI_MEMORY_DESCRIPTOR*)map->buffer,
+		&map->map_key,
+		&map->descriptor_size,
+		&map->descriptor_version);
 }
