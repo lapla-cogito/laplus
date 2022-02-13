74607fac5ef9b36ab98c441c73c5546d624133e6
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-02-13 14:08:31 +0900

    カーネル読み込み

--- laplusLoader/main.c
+++ laplusLoader/main.c
@@ -5,6 +5,30 @@ EFI_STATUS EFIAPI UefiMain(
 	EFI_HANDLE image_handle,
 	EFI_SYSTEM_TABLE* system_table) {
 	Print(L"Hello,World!\n This is laplus OS!");
+
+	//カーネルファイルを読み込む部分
+	EFI_FILE_PROTOCOL* kernel_file;
+	root_dir->Open(
+		root_dir, &kernel_file, L"\\kernel.elf",
+		EFI_FILE_MODE_READ, 0);
+
+	UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+	UINT8 file_info_buffer[file_info_size];
+	kernel_file->GetInfo(
+		kernel_file, &gEfiFileInfoGuid,
+		&file_info_size, file_info_buffer);
+
+	EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+	UINTN kernel_file_size = file_info->FileSize;
+
+	EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+	gBS->AllocatePages(
+		AllocateAddress, EfiLoaderData,
+		(kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+	kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+	Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+	//カーネル読み込み終了
+
 	while (1);
 	return EFI_SUCCESS;
 }
