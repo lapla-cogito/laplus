a4f2a59c77363f6252ed8c1c6ada8df02c0f4768
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-03-18 13:31:24 +0900

    Update cowsay.cpp

--- Apps/cowsay/cowsay.cpp
+++ Apps/cowsay/cowsay.cpp
@@ -1,7 +1,7 @@
 //cowsayコマンド
 #include <cstudio>
 #include <cstdlib>
-
+#include <string>
 
 extern "C" void main(int argc, char** argv) {
 	if (argc < 1) {
@@ -9,5 +9,19 @@ extern "C" void main(int argc, char** argv) {
 		exit(1);
 	}
 
+	std::string str;
+	//スペースが入っていた時用にconcat処理
+	if (argc == 1) { str = argv[1]; }
+	else {
+		for (int i = 1; i < argc; ++i) {
+			str += argv[i];
+			if (i != argc - 1) { str += " "; }
+		}
+	}
 
+	//from here, output cowsay
+	printf(" ");
+	for (int i = 0; i < str.length() + 2; ++i) { printf("_"); }
+	printf(); puts();
+	std::count << "< " << str << " >" << endl;
 }
