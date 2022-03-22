9a27577189eb507ba31300790715430fc56a934f
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-03-22 14:33:47 +0900

    Update main.c

--- laplusLoader/main.c
+++ laplusLoader/main.c
@@ -47,34 +47,57 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
 
 const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
 	switch (type) {
-	case EfiReservedMemoryType: return L"EfiReservedMemoryType";
-	case EfiLoaderCode: return L"EfiLoaderCode";
-	case EfiLoaderData: return L"EfiLoaderData";
-	case EfiBootServicesCode: return L"EfiBootServicesCode";
-	case EfiBootServicesData: return L"EfiBootServicesData";
-	case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
-	case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
-	case EfiConventionalMemory: return L"EfiConventionalMemory";
-	case EfiUnusableMemory: return L"EfiUnusableMemory";
-	case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
-	case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
-	case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
-	case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
-	case EfiPalCode: return L"EfiPalCode";
-	case EfiPersistentMemory: return L"EfiPersistentMemory";
-	case EfiMaxMemoryType: return L"EfiMaxMemoryType";
-	default: return L"InvalidMemoryType";
+	case EfiReservedMemoryType:
+		return L"EfiReservedMemoryType";
+	case EfiLoaderCode:
+		return L"EfiLoaderCode";
+	case EfiLoaderData:
+		return L"EfiLoaderData";
+	case EfiBootServicesCode:
+		return L"EfiBootServicesCode";
+	case EfiBootServicesData:
+		return L"EfiBootServicesData";
+	case EfiRuntimeServicesCode:
+		return L"EfiRuntimeServicesCode";
+	case EfiRuntimeServicesData:
+		return L"EfiRuntimeServicesData";
+	case EfiConventionalMemory:
+		return L"EfiConventionalMemory";
+	case EfiUnusableMemory:
+		return L"EfiUnusableMemory";
+	case EfiACPIReclaimMemory:
+		return L"EfiACPIReclaimMemory";
+	case EfiACPIMemoryNVS:
+		return L"EfiACPIMemoryNVS";
+	case EfiMemoryMappedIO:
+		return L"EfiMemoryMappedIO";
+	case EfiMemoryMappedIOPortSpace:
+		return L"EfiMemoryMappedIOPortSpace";
+	case EfiPalCode:
+		return L"EfiPalCode";
+	case EfiPersistentMemory:
+		return L"EfiPersistentMemory";
+	case EfiMaxMemoryType:
+		return L"EfiMaxMemoryType";
+	default:
+		return L"InvalidMemoryType";
 	}
 }
 
 const CHAR16* GetPixelFormatUnicode(EFI_PIXEL_GRAPHICS_FORMAT fmt) {
 	switch (fmt) {
-	case PixelRedGreenBlueReserved8bitPerColor: return L"PixelRedGreenBlueReserved 8bitPerColor";
-	case PixelBlueGreenRedReserved8bitPerColor: return L"PixelBlueGreenRedReservedPerColor";
-	case PixelBitMask: return L"PixelBitMask";
-	case PixelBitOnly:return L"PixelBitOnly";
-	case PixelFormatMax:return L"PixelFormatMax";
-	default: return L"Invalid PixelFormat";
+	case PixelRedGreenBlueReserved8bitPerColor:
+		return L"PixelRedGreenBlueReserved 8bitPerColor";
+	case PixelBlueGreenRedReserved8bitPerColor:
+		return L"PixelBlueGreenRedReservedPerColor";
+	case PixelBitMask:
+		return L"PixelBitMask";
+	case PixelBitOnly:
+		return L"PixelBitOnly";
+	case PixelFormatMax:
+		return L"PixelFormatMax";
+	default:
+		return L"Invalid PixelFormat";
 	}
 }
 
