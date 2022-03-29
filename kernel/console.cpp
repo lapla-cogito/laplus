6179be87272e73f0127101903c9ec47a2dc537ba
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-03-29 16:58:01 +0900

    console

--- kernel/console.cpp
+++ kernel/console.cpp
@@ -1,39 +1,94 @@
 #include "console.hpp"
+#include <cstring>
+#include "font.hpp"
+#include "layer.hpp"
 
-//コンストラクタ定義
-Console::Console(PixelWriter& writer, const PixelColor& fg_col, const PixelColor& bg_col) : writer_{ writer }, fg_col_{ fg_col }, bg_col_{ bg_col }, buffer_{}, cursor_row_{ 0 }, cursor_column_{ 0 } {}
-//コンストラクタ定義終了
-
-//改行
-void Console::Newline() {
-	cursor_column_ = 0;
-	if (cursor_row_ < kRows - 1) {
-		++cursor_row_;
-	}
-	else {
-		for (int y = 0; y < 16 * kRows; ++y) {
-			for (int x = 0; x < 8 * kColumns; ++x) { writer_.write(x, y, bg_col_); }
-		}
-
-		for (int row = 0; row < kROws - 1; ++row) {
-			memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
-			WriteString(writer_, 0, 16 * row, buffer_[row], fg_col_);
-		}
-		memset(buffer_[kRows - 1], 0, kColumns + 1);
-	}
+Console::Console(const PixelColor& fg_color, const PixelColor& bg_color)
+	: writer_{ nullptr }, window_{}, fg_color_{ fg_color }, bg_color_{ bg_color },
+	buffer_{}, cursor_row_{ 0 }, cursor_column_{ 0 }, layer_id_{ 0 } {
 }
-//改行終了
 
-void Concole::PutString(const char* s) {
+void Console::PutString(const char* s) {
 	while (*s) {
 		if (*s == '\n') {
 			Newline();
 		}
-		else if (cursor_column < kColumns - 1) {
-			WriteAscii(writer_, 8 * cursor_column, 16 * cursor_row_, *s, fg_col_);
+		else if (cursor_column_ < kColumns - 1) {
+			WriteAscii(*writer_, Vector2D<int>{8 * cursor_column_, 16 * cursor_row_}, * s, fg_color_);
 			buffer_[cursor_row_][cursor_column_] = *s;
 			++cursor_column_;
 		}
 		++s;
 	}
+	if (layer_manager) {
+		layer_manager->Draw(layer_id_);
+	}
+}
+
+void Console::SetWriter(PixelWriter* writer) {
+	if (writer == writer_) {
+		return;
+	}
+	writer_ = writer;
+	window_.reset();
+	Refresh();
+}
+
+void Console::SetWindow(const std::shared_ptr<Window>& window) {
+	if (window == window_) {
+		return;
+	}
+	window_ = window;
+	writer_ = window->Writer();
+	Refresh();
+}
+
+void Console::SetLayerID(unsigned int layer_id) {
+	layer_id_ = layer_id;
+}
+
+unsigned int Console::LayerID() const {
+	return layer_id_;
+}
+
+void Console::Newline() {
+	cursor_column_ = 0;
+	if (cursor_row_ < kRows - 1) {
+		++cursor_row_;
+		return;
+	}
+
+	if (window_) {
+		Rectangle<int> move_src{ {0, 16}, {8 * kColumns, 16 * (kRows - 1)} };
+		window_->Move({ 0, 0 }, move_src);
+		FillRectangle(*writer_, { 0, 16 * (kRows - 1) }, { 8 * kColumns, 16 }, bg_color_);
+	}
+	else {
+		FillRectangle(*writer_, { 0, 0 }, { 8 * kColumns, 16 * kRows }, bg_color_);
+		for (int row = 0; row < kRows - 1; ++row) {
+			memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
+			WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
+		}
+		memset(buffer_[kRows - 1], 0, kColumns + 1);
+	}
+}
+
+void Console::Refresh() {
+	FillRectangle(*writer_, { 0, 0 }, { 8 * kColumns, 16 * kRows }, bg_color_);
+	for (int row = 0; row < kRows; ++row) {
+		WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
+	}
+}
+
+Console* console;
+
+namespace {
+	char console_buf[sizeof(Console)];
+}
+
+void InitializeConsole() {
+	console = new(console_buf) Console{
+	  kDesktopFGColor, kDesktopBGColor
+	};
+	console->SetWriter(screen_writer);
 }
--- kernel/console.hpp
+++ kernel/console.hpp
@@ -1,16 +1,31 @@
 #pragma once
+#include <memory>
 #include "graphics.hpp"
+#include "window.hpp"
 
-class Concole {
-private:
-	void Newline();
-	PixelWriter& writer_;
-	const PixelColor fg_col_, bg_col_;
-	char buffer_[kRows][kColumns + 1];
-	int cursor_row_, cursor_column;
-
+class Console {
 public:
 	static const int kRows = 25, kColumns = 80;
-	Console(PixelWriter& writer, const PixelColor& fg_col, const PixelColor& bg_col);
+
+	Console(const PixelColor& fg_color, const PixelColor& bg_color);
 	void PutString(const char* s);
+	void SetWriter(PixelWriter* writer);
+	void SetWindow(const std::shared_ptr<Window>& window);
+	void SetLayerID(unsigned int layer_id);
+	unsigned int LayerID() const;
+
+private:
+	void Newline();
+	void Refresh();
+
+	PixelWriter* writer_;
+	std::shared_ptr<Window> window_;
+	const PixelColor fg_color_, bg_color_;
+	char buffer_[kRows][kColumns + 1];
+	int cursor_row_, cursor_column_;
+	unsigned int layer_id_;
 };
+
+extern Console* console;
+
+void InitializeConsole();
