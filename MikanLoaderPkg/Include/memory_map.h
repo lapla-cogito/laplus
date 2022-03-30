6b0988af7e37486fa4261638a5798731640bee2b
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-03-31 02:37:35 +0900

    Include

---
+++ MikanLoaderPkg/Include/memory_map.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <Uefi.h>
+
+struct MemoryMap {
+	UINTN buffer_size;
+	VOID* buffer;
+	UINTN map_size;
+	UINTN map_key;
+	UINTN descriptor_size;
+	UINT32 descriptor_version;
+};
