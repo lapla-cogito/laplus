a4240be462f5f94fa2cc0ffa0305692df0dddcc4
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-03-31 04:06:40 +0900

    del

--- MikanLoaderPkg/mmap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Library/PrintLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/UefiLib.h>
-#include <Uefi.h>
-#include "Include/memory_map.h"
-
-EFI_STATUS GetMemoryMap(struct MemoryMap* map);
-EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file);
