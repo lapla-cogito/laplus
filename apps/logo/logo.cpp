8baddd60a16dd68c892dcc466f027491f81f7558
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-04-01 11:32:33 +0900

    rev

---
+++ apps/logo/logo.cpp
@@ -0,0 +1,18 @@
+//ターミナル上でのロゴ表示
+#include <cstdio>
+#include <cstdlib>
+
+extern "C" void main(int argc, char** argv) {
+	fprintf(L" __                                                                __                                                         \n");
+	fprintf(L"|  \                                                              |  \                                                        \n");
+	fprintf(L"| $$                    ______                ______              | $$             __    __               _______             \n");
+	fprintf(L"| $$                   |      \              /      \             | $$            |  \  |  \             /       \            \n");
+	fprintf(L"| $$                    \$$$$$$\            |  $$$$$$\            | $$            | $$  | $$            |  $$$$$$$            \n");
+	fprintf(L"| $$                   /      $$            | $$  | $$            | $$            | $$  | $$             \$$    \             \n");
+	fprintf(L"| $$_____             |  $$$$$$$            | $$__/ $$            | $$            | $$__/ $$             _\$$$$$$\            \n");
+	fprintf(L"| $$     \             \$$    $$            | $$    $$            | $$             \$$    $$            |       $$            \n");
+	fprintf(L" \$$$$$$$$              \$$$$$$$            | $$$$$$$              \$$              \$$$$$$              \$$$$$$$             \n");
+	fprintf(L"                                            | $$                                                                              \n");
+	fprintf(L"                                            | $$                                                                              \n");
+	fprintf(L"                                             \$$                                                                              \n");
+}
