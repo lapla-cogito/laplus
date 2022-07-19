9039e89f6797b842f1e0f2918c2108b4320c7809
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-07-19 22:59:41 +0900

    Update cowsay.cpp

--- apps/cowsay/cowsay.cpp
+++ apps/cowsay/cowsay.cpp
@@ -1,38 +1,45 @@
-//cowsayコマンド
+/**
+ * @file cowsay.cpp
+ *
+ * @brief cowsayコマンド
+ *
+ */
 
+#include "../syscall.h"
 #include <cstdio>
 #include <cstdlib>
-#include <string>
 #include <string.h>
-#include "../syscall.h"
+#include <string>
 
 using namespace std;
 
 //うし
-char ushi[] = "        \\   ^__^\n         \\  (oo)\\\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||\n";
+char ushi[] = "        \\   ^__^\n         \\  (oo)\\\n            (__)\\      "
+              " )\\/\\\n                ||----w |\n                ||     ||\n";
 
-extern "C" void main(int argc, char** argv) {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-		exit(1);
-	}
+extern "C" void main(int argc, char **argv) {
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+        exit(1);
+    }
 
-	char str[] = "";
+    char str[] = "";
 
-	//スペースが入っていた時用にconcat処理
-	if (argc == 1) { strcpy(str, argv[1]); }
-	else {
-		for (int i = 1; i < argc; ++i) {
-			strcat(str, argv[i]);
-			if (i != argc - 1) { strcat(str, " "); }
-		}
-	}
+    //スペースが入っていた時用にconcat処理
+    if(argc == 1) {
+        strcpy(str, argv[1]);
+    } else {
+        for(int i = 1; i < argc; ++i) {
+            strcat(str, argv[i]);
+            if(i != argc - 1) { strcat(str, " "); }
+        }
+    }
 
-	//from here, output cowsay
-	printf(" ");
-	for (int i = 0; i < strlen(str) + 2; ++i) { printf("_"); }
-	printf(" \n< %s >\n ", str);
+    // from here, output cowsay
+    printf(" ");
+    for(int i = 0; i < strlen(str) + 2; ++i) { printf("_"); }
+    printf(" \n< %s >\n ", str);
 
-	for (int i = 0; i < strlen(str) + 2; ++i) { printf("-"); }
-	printf(" \n%s", ushi);
+    for(int i = 0; i < strlen(str) + 2; ++i) { printf("-"); }
+    printf(" \n%s", ushi);
 }
