a0a0e38360438813be5b50518cfe0a5a05899dc5
laplaci_an <39644970+laplaciancpp@users.noreply.github.com>
2022-07-21 23:28:16 +0900

    Update udps.cpp

--- apps/udps/udps.cpp
+++ apps/udps/udps.cpp
@@ -18,4 +18,36 @@ extern "C" int main(int argc, char *argv[]) {
     }
 
     printf("socket success: soc=%d\n", soc);
+
+    self.sin_family = AF_INET;
+    self.sin_addr = INADDR_ANY;
+    self.sin_port = hton16(7); /*UDPのechoの識別ポート番号は7*/
+
+    if(bind(soc, (struct socketaddr *)&self, sizeof(self)) == -1) {
+        printf("bind failure\n");
+        close(soc);
+        exit(-1);
+    }
+
+    addr = (unsigned char *)&self.sin_addr;
+
+    printf("bind success: self=%d.%d.%d.%d:%d\n", addr[0], addr[1], addr[2],
+           addr[3], ntoh(self.sin_port));
+
+    while(1) {
+        peern = sizeof(peer);
+        ret = recvfrom(soc, buf, sizeof(buf), (struct sockaddr *)&peer, &peern);
+        if(ret <= 0) {
+            printf("EOF\n");
+            break;
+        }
+
+        addr=(unsigned char *)&peer.sin_addr;
+        printf("recvfrom success: %d bytes data received, peer=%d.%d.%d.%d:%d",ret,addr[0],addr[1],addr[2],addr[3],ntoh(peer.sin_port));
+
+        sendto(soc,buf,ret,(struct sockaddr *)&peer,peern);
+    }
+
+    close(soc);
+    exit(-1);
 }
